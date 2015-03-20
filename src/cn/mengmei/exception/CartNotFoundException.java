db346169f10909d65baa4e6fbef71e76b4ee4537
MeiMeng <mengmei219@qq.com>
2015-03-20 22:27:40 +0800

    Create CartNotFoundException.java
    13.开发自定义异常类：CartNotFoundException 。

---
+++ src/cn/mengmei/exception/CartNotFoundException.java
@@ -0,0 +1,31 @@
+package cn.mengmei.exception;
+
+public class CartNotFoundException extends Exception {
+
+	public CartNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartNotFoundException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CartNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
