60251e34515b809de79453f0bed9cd483d0a63c9
MeiMeng <mengmei219@qq.com>
2015-03-20 22:28:54 +0800

    Create NumberIllegal.java
    16.开发NumberIllegal异常类。

---
+++ src/cn/mengmei/exception/NumberIllegal.java
@@ -0,0 +1,31 @@
+package cn.mengmei.exception;
+
+public class NumberIllegal extends Exception {
+
+	public NumberIllegal() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NumberIllegal(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NumberIllegal(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NumberIllegal(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NumberIllegal(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
